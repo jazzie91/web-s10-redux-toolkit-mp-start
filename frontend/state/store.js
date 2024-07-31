f94f389779fbb2dacf6183a6c313eee5a60f0437
Jazzie91 <jasminecarter88@gmail.com>
2024-07-31 01:43:22 -0500

    finished

--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,1 +1,8 @@
-// ✨ create your `store` in this module
+import { configureStore } from "@reduxjs/toolkit";
+import quoteReducer from './quotesSlice'
+
+export const store = configureStore({
+    reducer: {
+        quotesState: quoteReducer,
+    } 
+})
